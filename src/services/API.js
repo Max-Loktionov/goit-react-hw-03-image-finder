d00d20d01293ce157b6c17433b40b6f587078292
Max-Loktionov <studreno@gmail.com>
2022-07-20 19:50:56 +0300

    fix API

--- src/services/API.js
+++ src/services/API.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 const KEY = '27626475-8422ee6256ea07f97d3a4bc44';
-axios.defaults = `https://pixabay.com/api/?key=${KEY}&image_type=photo&orientation=horizontal`;
 async function getGallery(newQuery, newPage) {
   const response = await axios.get(
-    `&q=${newQuery}&page=${newPage}&per_page=12`,
-    (newQuery, newPage)
+    `https://pixabay.com/api/?key=${KEY}&image_type=photo&orientation=horizontal&q=${newQuery}&page=${newPage}&per_page=12`
   );
   return response.data;
 }
